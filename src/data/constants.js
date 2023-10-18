b33a7a009388447ed33ca63e554938a587d3693d
mallikanns <137501517+mallikanns@users.noreply.github.com>
2023-10-19 00:11:56 +0700

    update skill and cv

--- src/data/constants.js
+++ src/data/constants.js
@@ -81,6 +81,11 @@ export const skills = [
           "https://camo.githubusercontent.com/93b32389bf746009ca2370de7fe06c3b5146f4c99d99df65994f9ced0ba41685/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f676574706f73746d616e2f676574706f73746d616e2d69636f6e2e737667",
       },
       {
+        name: "SQLite",
+        image:
+          "http://robtiffany.com/wp-content/uploads/2013/08/sqlite-logo.png",
+      },
+      {
         name: "Figma",
         image:
           "https://camo.githubusercontent.com/ed93c2b000a76ceaad1503e7eb9356591b885227e82a36a005b9d3498b303ba5/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6669676d612f6669676d612d69636f6e2e737667",
@@ -97,15 +102,15 @@ export const skills = [
 export const projects = [
   {
     id: 0,
-    title: "FitUp",
+    title: "FitUp (In progress)",
     date: "Aug 2023 - Oct 2023",
     description:
       "FitUp is a full-stack exercise-tracking web application (CRUD) that motivates users with monetary rewards and offers comprehensive data visualization tools.",
     image:
       "https://i.postimg.cc/cC08ZPDY/project-fitup.png",
     tags: ["MongoDB", "Node Js", "Express Js", "React Js", "Tailwind"],
-    github: "#",
-    webapp: "#",
+    // github: "#",
+    // webapp: "#",
   },
   {
     id: 1,
@@ -143,7 +148,7 @@ export const projects = [
       "https://i.postimg.cc/Dzzt8zNy/findyourhat-project.png",
     tags: ["JavaScript"],
     github: "https://github.com/mallikanns/find-your-hat",
-    webapp: "https://github.com/mallikanns/find-your-hat",
+    webapp: "https://replit.com/@MallikaNilnoisr/19-find-your-hat",
   },
   {
     id: 4,
